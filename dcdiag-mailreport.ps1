94eb24a0af6cd43b4c0fdf3121f419d383eb896d
长颈鹿 <37991589+Ms-V@users.noreply.github.com>
2019-04-12 16:25:32 +0800

    just running this one is enough
    just running this one is enough

--- dcdiag-mailreport.ps1
+++ dcdiag-mailreport.ps1
@@ -3,7 +3,6 @@ dcdiag /a /q >> D:\dcdiagreport\dcdiagreport.txt
 $From="you mail"
 $To="Addressee "
 $Subject="mail Title" 
-#$Body=Get-Content -Encoding UTF8 "D:\dcdiagreport\dcdiagreport.txt" -RAW
 $Body=Get-Content "The dcdiag test txt path" -RAW
 $smtpServer="smtp mail server"
 $smtpPort=smtp port
@@ -12,8 +11,6 @@ $password="password"
 $SMTPMessage=New-Object System.Net.Mail.MailMessage($From, $To, $Subject,$Body) 
 $SMTPClient=New-Object Net.Mail.SmtpClient($smtpServer, $SmtpPort)
 $SMTPMessage.IsBodyHtml = $false
-#$SMTPMessage.SubjectEncoding = [system.Text.Encoding]::GetEncoding("utf-16")
-#$SMTPMessage.BodyEncoding = [system.Text.Encoding]::GetEncoding("utf-16") 
 $SMTPClient.EnableSsl=$ture #ssl is enable or not
 $SMTPClient.Credentials=New-Object System.Net.NetworkCredential($username, $password); 
 $SMTPClient.Send($SMTPMessage) #send
