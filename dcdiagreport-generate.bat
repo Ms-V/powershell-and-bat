eb7eb940367616f541e905a8cf01601bdaf58397
长颈鹿 <37991589+Ms-V@users.noreply.github.com>
2019-04-12 16:22:25 +0800

    Delete dcdiagreport-generate.bat

--- dcdiagreport-generate.bat
+++
@@ -1,5 +0,0 @@
-set date=%date:~0,15%
-set time=%time:~0,11%
-echo %date% > D:\dcdiagreport\dcdiagreport.txt
-echo %time% >> D:\dcdiagreport\dcdiagreport.txt
-dcdiag /a /q >> D:\dcdiagreport\dcdiagreport.txt
