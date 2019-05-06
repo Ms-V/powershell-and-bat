9e55466a95aa6f11ecc7d10217049ace7be19f9e
长颈鹿 <37991589+Ms-V@users.noreply.github.com>
2019-05-06 11:40:50 +0800

    增加了等待五秒，同时解决了有时，win10显示pro不显示Professional时也能激活不报错

--- auto-activation/auto-activation-jp.ps1
+++ auto-activation/auto-activation-jp.ps1
@@ -2,32 +2,31 @@ $win7 = "win7 key"
 $win10ent = "win10 ent key"
 $win10pro = "win10 pro key"
 $ver = Get-WmiObject -Class Win32_OperatingSystem | Select-Object -ExpandProperty Caption
-if($ver -like "*Windows 10*" -and $ver -like "*Professional*" -or "Pro")
+if($ver -like "*Windows 10*" -and ($ver -like "*Professional*" -or "*Pro*"))
 {
     Write-Host "The os is "$ver "AKA Win10 pro.start inject key"
     slmgr -ipk $win10pro
+    start-sleep -s 5
     Write-Host "Start activation"
     slmgr -ato
-    Read-Host
 }
 elseif($ver -like "*Windows 10*" -and $ver -like "*Enterprise*")
 {
     Write-Host "The os is "$ver "AKA Win10 Ent.start inject key"
     slmgr -ipk $win10ent
+    start-sleep -s 5
     Write-Host "Start activation"
     slmgr -ato
-    Read-Host
 }
 elseif($ver -like "*Windows 7*")
 {
     Write-Host "The os is "$ver "AKA Win7.start inject key"
     slmgr -ipk $win7
+    start-sleep -s 5
     Write-Host "Start activation"
     slmgr -ato
-    Read-Host
 }
 else
 {
     Write-Host "Error"
-    Read-Host
 }
