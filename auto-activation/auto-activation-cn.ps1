24f453edf3f5df5228e558f0151783dc301dec35
长颈鹿 <37991589+Ms-V@users.noreply.github.com>
2019-05-06 11:38:42 +0800

    增加了等待5秒，应为有时候速度太快还没输入key就开始激活了

--- auto-activation/auto-activation-cn.ps1
+++ auto-activation/auto-activation-cn.ps1
@@ -6,28 +6,27 @@ if($ver -like "*Windows 10*" -and $ver -like "*专业版*")
 {
     Write-Host "The os is "$ver "AKA Win10 pro.start inject key"
     slmgr -ipk $win10pro
+    start-sleep -s 5
     Write-Host "Start activation"
     slmgr -ato
-    Read-Host
 }
 elseif($ver -like "*Windows 10*" -and $ver -like "*企业版*")
 {
     Write-Host "The os is "$ver "AKA Win10 Ent.start inject key"
     slmgr -ipk $win10ent
+    start-sleep -s 5
     Write-Host "Start activation"
     slmgr -ato
-    Read-Host
 }
 elseif($ver -like "*Windows 7*")
 {
     Write-Host "The os is "$ver "AKA Win7.start inject key"
     slmgr -ipk $win7
+    start-sleep -s 5
     Write-Host "Start activation"
     slmgr -ato
-    Read-Host
 }
 else
 {
     Write-Host "Error"
-    Read-Host
 }
