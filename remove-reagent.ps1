63c746f0de17c7d5234dce6660e157368231785c
长颈鹿 <37991589+Ms-V@users.noreply.github.com>
2020-06-12 17:14:40 +0800

    解决用WIM+WDS部署的windows系统无法开启bitlocker
    Windows10笔记本无法开启bitlocker报错，系统找不到指定的文件时。就用这个，其实就是一删 系统盘-Windows-System32-Recovery中的ReAgent.xml就行

---
+++ remove-reagent.ps1
@@ -0,0 +1,15 @@
+﻿$g_wreagentdate=Get-Date((Get-Item $env:SystemDrive\Windows\System32\Recovery\ReAgent.xml).LastWriteTime) -Format "yyyy-MM-dd" #get reagent.xml last write date
+$nowdate=Get-Date -Format "yyyy-MM-dd-HH-mm" #get now date
+$r_wreagentdate="2020-06-03"#Package date 
+if($g_wreagentdate -eq $r_wreagentdate)
+{
+    rename-Item $env:SystemDrive\Windows\System32\Recovery\ReAgent.xml -NewName Reagent.xml$nowdate
+    Exit
+}
+else
+{
+    Write-Host "It's look like not the first time remove The ReAgent.xml file,please remove it Manual.`nThe Path is $env:SystemDrive\Windows\System32\Recovery\" -ForegroundColor Red -BackgroundColor Yellow
+    Write-Host "Enter key to exit"
+    Read-Host | Out-Null
+    Exit
+}
