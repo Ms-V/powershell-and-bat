5da309e56e932cb18b62524e925e9da590778bd5
长颈鹿 <37991589+Ms-V@users.noreply.github.com>
2020-06-15 17:52:07 +0800

    Update remove-reagent.ps1

--- remove-reagent.ps1
+++ remove-reagent.ps1
@@ -1,12 +1,12 @@
 ﻿$g_wreagentdate=Get-Date((Get-Item $env:SystemDrive\Windows\System32\Recovery\ReAgent.xml).LastWriteTime) -Format "yyyy-MM-dd" #get reagent.xml last write date
 $nowdate=Get-Date -Format "yyyy-MM-dd-HH-mm" #get now date
-$r_wreagentdate="2020-06-03"#Package date 
-if($g_wreagentdate -eq $r_wreagentdate)
+$r_wreagentdate="2020-06-03"#Package date #封装wim的日期
+if($g_wreagentdate -eq $r_wreagentdate)#判定日期是否是封装wim的日期，是的话直接删除
 {
     rename-Item $env:SystemDrive\Windows\System32\Recovery\ReAgent.xml -NewName Reagent.xml$nowdate
     Exit
 }
-else
+else#不是的话提示，手动去删除
 {
     Write-Host "It's look like not the first time remove The ReAgent.xml file,please remove it Manual.`nThe Path is $env:SystemDrive\Windows\System32\Recovery\" -ForegroundColor Red -BackgroundColor Yellow
     Write-Host "Enter key to exit"
