4b0752ed82d849561e777d9ec9b0f6a1224fe7ba
长颈鹿 <37991589+Ms-V@users.noreply.github.com>
2020-11-20 14:20:20 +0800

    一个被抛弃的更改更新设置和地址的PS1
    为什么会被抛弃？应为脚本是对于注册表进行编辑，但是运行后从gpedit.msc查看并没有更新组策略。但实际上更改已经生效了！这是微软有意设计的，为了避免一些问题。所以应为在gpedit.msc中查看与实际不符，会给接下来的运维工作带来不必要的麻烦所以抛弃了。

---
+++ set_update_address_xf.ps1
@@ -0,0 +1,19 @@
+﻿$updateregedit="HKLM:SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\"
+$updateauregedit="HKLM:SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"
+
+New-ItemProperty -Path $updateregedit -Name UpdateServiceUrlAlternate -Value "http://11.11.11.11:8530" -PropertyType String -Force
+New-ItemProperty -Path $updateregedit -Name WUServer -Value "http://11.11.11.11:8530" -PropertyType String -Force
+New-ItemProperty -Path $updateregedit -Name WUStatusServer -Value "http://11.11.11.11:8530" -PropertyType String -Force
+
+New-ItemProperty -Path $updateauregedit -Name AUOptions -Value 4 -PropertyType DWord -Force
+New-ItemProperty -Path $updateauregedit -Name AutomaticMaintenanceEnabled -Value 1 -PropertyType DWord -Force
+New-ItemProperty -Path $updateauregedit -Name DetectionFrequency -Value 4 -PropertyType DWord -Force
+New-ItemProperty -Path $updateauregedit -Name DetectionFrequencyEnabled -Value 1 -PropertyType DWord -Force
+New-ItemProperty -Path $updateauregedit -Name NoAutoRebootWithLoggedOnUsers -Value 1 -PropertyType DWord -Force
+New-ItemProperty -Path $updateauregedit -Name NoAutoUpdate -Value 0 -PropertyType DWord -Force
+New-ItemProperty -Path $updateauregedit -Name ScheduledInstallDay -Value 0 -PropertyType DWord -Force
+New-ItemProperty -Path $updateauregedit -Name ScheduledInstallEveryWeek -Value 1 -PropertyType DWord -Force
+New-ItemProperty -Path $updateauregedit -Name ScheduledInstallTime -Value 10 -PropertyType DWord -Force
+New-ItemProperty -Path $updateauregedit -Name UseWUServer -Value 1 -PropertyType DWord -Force
+
+
